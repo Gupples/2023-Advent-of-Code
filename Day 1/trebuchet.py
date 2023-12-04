4e004f0260a20e9f53ca2c7146dfc58b81a6ed34
Gupples <85248061+Gupples@users.noreply.github.com>
2023-12-04 14:24:17 -0700

    Added creator name and program description

--- Day 1/trebuchet.py
+++ Day 1/trebuchet.py
@@ -1,1 +1,6 @@
-# 
+# Creator: Jared Davey
+# Description: This program will read a file. For each line in the file, it 
+#   will store the first and last numerical character in an array, calculate
+#   the sum of those numbers, and then display the sum.
+
+ 
