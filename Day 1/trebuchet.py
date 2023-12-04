9c5b60699dc1ae2c6471726e66459f87fe928065
Gupples <85248061+Gupples@users.noreply.github.com>
2023-12-04 15:04:05 -0700

    Finished first draft of trebuchet.py

--- Day 1/trebuchet.py
+++ Day 1/trebuchet.py
@@ -3,4 +3,32 @@
 #   will store the first and last numerical character in an array, calculate
 #   the sum of those numbers, and then display the sum.
 
- 
+def main():
+    calibration_lines = read_file()
+
+def read_file():
+    filename = input("Enter the calibration file name.")
+    try:
+        with open(filename, 'r') as file:
+            lines = []
+            text = file.read()
+            for line in text:
+                lines.append(line)
+            return lines
+    except:
+        print("Could not read the file.")
+        return []
+    
+def calculate_values(lines):
+    values = []
+    for ine in lines:
+        number_first = '_'
+        number_last = "_"
+        for character in line:
+            if character.isnumeric():
+                if number_first == "_":
+                    number_first = character
+                number_last = character
+            value_string = number_first + number_last
+            values.append(int(value_string))
+        print(sum(values))
