96f16e8524e1b2e2e9dedb6f299e2cea14c850e3
Gupples <85248061+Gupples@users.noreply.github.com>
2023-12-06 17:39:48 -0700

    Edited to enable part 2. Haven't started yet.

--- Day 1/trebuchet.py
+++ Day 1/trebuchet.py
@@ -4,10 +4,53 @@
 #   the sum of those numbers, and then display the sum.
 
 def main():
+
+    mode = input("What part are you running? (1/2)")
+    
+    # Get the filename from the user
     filename = input("Enter the calibration file name. ")
+
+    if mode == 1:
+        answer = part_one(filename)
+    elif mode == 2:
+        answer = part_two(filename)
+    else:
+        print(f"Sorry, {mode} was not a mode.")
+    
+def part_one(filename):
+
+    # Read the .txt file.
     try:
         with open(filename, 'r') as file:
+
+            # Create an array to hold the values.
             values = []
+
+            # Find the values.
+            for line in file:
+                number_first = '_'
+                number_last = "_"
+                for character in line:
+                    if character.isnumeric():
+                        if number_first == "_":
+                            number_first = character
+                        number_last = character
+                    value_string = number_first + number_last
+                values.append(int(value_string))
+            print(sum(values))
+    except:
+        print(f"File '{filename}' could not be read.")
+
+def part_two(filename):
+    
+    # Read the .txt file.
+    try:
+        with open(filename, 'r') as file:
+
+            # Create an array to hold the values.
+            values = []
+
+            # Find the values.
             for line in file:
                 number_first = '_'
                 number_last = "_"
