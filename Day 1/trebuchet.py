ec52c39ac156358f386a8eb96ddd2765b30eb1d0
Gupples <85248061+Gupples@users.noreply.github.com>
2023-12-06 17:00:51 -0700

    Finished for part 1.

--- Day 1/trebuchet.py
+++ Day 1/trebuchet.py
@@ -4,36 +4,23 @@
 #   the sum of those numbers, and then display the sum.
 
 def main():
-    calibration_lines = read_file()
-    if len(calibration_lines) > 0:
-        calculate_values(calibration_lines)
-
-def read_file():
     filename = input("Enter the calibration file name. ")
     try:
         with open(filename, 'r') as file:
-            lines = []
-            text = file.read()
-            for line in text:
-                lines.append(line)
-            return lines
+            values = []
+            for line in file:
+                number_first = '_'
+                number_last = "_"
+                for character in line:
+                    if character.isnumeric():
+                        if number_first == "_":
+                            number_first = character
+                        number_last = character
+                    value_string = number_first + number_last
+                values.append(int(value_string))
+            print(sum(values))
     except:
-        print("Could not read the file.")
-        return []
-    
-def calculate_values(lines):
-    values = []
-    for ine in lines:
-        number_first = '_'
-        number_last = "_"
-        for character in line:
-            if character.isnumeric():
-                if number_first == "_":
-                    number_first = character
-                number_last = character
-            value_string = number_first + number_last
-            values.append(int(value_string))
-        print(sum(values))
+        print(f"File '{filename}' could not be read.")
 
 if __name__ == "__main__":
     main()
